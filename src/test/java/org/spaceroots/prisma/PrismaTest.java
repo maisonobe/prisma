afa74b7a2d071e4e822769fd0b5f1ffd9fd4a189
Luc Maisonobe <luc@orekit.org>
2024-11-24 18:04:46 +0100

    New measurements.

--- src/test/java/org/spaceroots/prisma/PrismaTest.java
+++ src/test/java/org/spaceroots/prisma/PrismaTest.java
@@ -1,6 +1,7 @@
 package org.spaceroots.prisma;
 
 import org.hipparchus.util.FastMath;
+import org.hipparchus.util.SinCos;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,9 +13,31 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class PrismaTest {
+
+    @Test
+    public void testTmp() {
+        final SinCos sc = FastMath.sinCos(FastMath.toRadians(60));
+        int count = 0;
+        for (double[] dh : new double[][] {
+            { 20, -3.7 }, { 20, -3.2 }, { 20, -2.8 }, { 20, -2.5 }, { 12,  0.0 }, { 15,  0.0 }, {  8,  2.5 }, {  8,  2.8 },
+            {  8,  3.2 }, { 12,  2.6 }, { 12,  3.1 }, { 12,  3.5 }, { 15,  3.2 }, { 15,  3.6 }, { 20,  2.8 }, { 20,  3.2 },
+            { 20,  3.7 }, { 20,  4.1 }, { 20,  4.5 }, { 20,  5.0 }, { 20,  5.4 }, { 20,  5.8 }, { 15,  7.5 }, { 20,  6.7 },
+            { 20,  7.2 }, { 20,  7.6 }, { 20,  8.0 }, { 20,  8.5 }, { 20,  8.9 }, { 20,  9.3 }, { 20,  9.8 }, { 20, 10.0 }
+        }) {
+            final double d = dh[0];
+            final double h = dh[1];
+            final double r = 20.03;
+            double m = 2 * r * SinCos.sum(sc, sc).sin() + (d * (1 + sc.sin()) - (d + 2 * h) * sc.cos()) / sc.sin();
+            double l = (2 * h + d * (1 - sc.cos())) / (2 * sc.sin());
+            if (l >= 0 && l <= 34.6) {
+                System.out.format(Locale.US, "%2d %3.0f %4.1f %6.3f %6.3f%n", ++count, d, h, l, m);
+            }
+        }
+    }
 
     @Test
     public void test606060() throws IOException {
@@ -102,6 +125,26 @@ public class PrismaTest {
     }
 
     @Test
+    public void testRealRuleSeventhMeasurementsA() throws IOException {
+        doTest("real-rule-seventh-measurements-endpoint-A.txt", 20.023, 60.025, 60.067, 59.908, 1.0e-3, 1.0e-3);
+    }
+
+    @Test
+    public void testRealRuleSeventhMeasurementsB() throws IOException {
+        doTest("real-rule-seventh-measurements-endpoint-B.txt", 20.062, 60.019, 60.077, 59.904, 1.0e-3, 1.0e-3);
+    }
+
+    @Test
+    public void testRealRuleEighthMeasurementsA() throws IOException {
+        doTest("real-rule-eighth-measurements-endpoint-A.txt", 20.008, 60.002, 60.069, 59.930, 1.0e-3, 1.0e-3);
+    }
+
+    @Test
+    public void testRealRuleEighthMeasurementsB() throws IOException {
+        doTest("real-rule-eighth-measurements-endpoint-B.txt", 20.037, 60.022, 60.068, 59.910, 1.0e-3, 1.0e-3);
+    }
+
+    @Test
     public void testInexistentFile() {
         try {
             new Prisma(findPath("prismatic-rule-60-60-60.txt").resolve("inexistent"));
@@ -147,18 +190,18 @@ public class PrismaTest {
     }
 
     @Test
-    public void testMainWithOptionsSixthA() throws IOException {
+    public void testMainWithOptionsEighthA() throws IOException {
         Prisma.main(new String[] {
             "--show-evaluations", "--residuals", "--plot",
-            findPath("real-rule-sixth-measurements-endpoint-A.txt").toString()
+            findPath("real-rule-eighth-measurements-endpoint-A.txt").toString()
         });
     }
 
     @Test
-    public void testMainWithOptionsSixthB() throws IOException {
+    public void testMainWithOptionsEighthB() throws IOException {
         Prisma.main(new String[] {
             "--show-evaluations", "--residuals", "--plot",
-            findPath("real-rule-sixth-measurements-endpoint-B.txt").toString()
+            findPath("real-rule-eighth-measurements-endpoint-B.txt").toString()
         });
     }
 
